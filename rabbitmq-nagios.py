9607c93deeb7fd6ef2c026f9b515d950fea5b713
kmcminn <tenyou@gmail.com>
2013-02-09 00:54:54 +0000

    refactor to use pynagios

--- rabbitmq-nagios.py
+++ rabbitmq-nagios.py
@@ -1,76 +1,142 @@
-#!/usr/bin/env /usr/bin/python  
-  
-from optparse import OptionParser  
-import urllib2  
-import json  
+#!/usr/bin/env /usr/bin/python
+
+from optparse import OptionParser
+import urllib2
+import json
 from pprint import pprint
-import time  
-import datetime  
-  
-def getOptions():  
-    arguments = OptionParser()  
-    arguments.add_option("--host", dest="host", help="Host rabbitmq is running on", type="string", default="localhost")  
-    arguments.add_option("--queue", dest="queue", help="Name of the queue in inspect", type="string", default="celery")  
-    arguments.add_option("--username", dest="username", help="RabbitMQ API username", type="string", default="guest")  
-    arguments.add_option("--password", dest="password", help="RabbitMQ API password", type="string", default="guest")  
-    arguments.add_option("--port", dest="port", help="RabbitMQ API port", type="string", default="15672")  
-    arguments.add_option("--vhost", dest="vhost", help="RabbitMQ vhost", type="string", default='%2F')
-      
-    arguments.add_option("--warning-queue-size", dest="warn_queue", help="Size of the queue to alert as warning", type="int", default=10000)  
-    arguments.add_option("--critical-queue-size", dest="crit_queue", help="Size of the queue to alert as critical", type="int", default=20000)  
-  
-    arguments.add_option("--warning-seconds", dest="warn_seconds", help="Last event processes in seconds ago to alert as warning", type="int", default=3600)  
-    arguments.add_option("--critical-seconds", dest="crit_seconds", help="Last event processes in seconds ago to alert as critical", type="int", default=14400)  
-          
-    return arguments.parse_args()[0]  
+from pynagios import Plugin, make_option
+import time
+import datetime
 
-def doApiGet(a, u, p):
-    # handle HTTP Auth
-    password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
-    top_level_url = a
-    password_mgr.add_password(None, top_level_url, u, p)
-    handler = urllib2.HTTPBasicAuthHandler(password_mgr)
-    opener = urllib2.build_opener(handler)
 
-    response = None
-    try:
-        request = opener.open(url)
-        response = request.read()
-        request.close()
-    except urllib2.HTTPError, e:
-        print "Error code %s hitting %s" % (e.code, url)
-        return False
 
-    return response
+class RabbitCheck(Plugin):
+    """
+    performs a nagios compliant check on a single queue and
+    attempts to catch all errors. expected usage is with a critical threshold of 0
+    """
 
-  
-if __name__ == '__main__':  
-      
-    options = getOptions()      
-      
-    url = "http://%s:%s/api/queues/%s/%s" % (options.host, options.port, options.vhost, options.queue)  
-      
-    content = doApiGet(url, options.username, options.password)
-          
-    data = json.loads(content)  
-      
-    #pprint(data)  
-      
-    num_messages = data["messages"]
-    if num_messages > options.crit_queue or num_messages > options.warn_queue:  
-        print "%s messages in %s queue" % (num_messages, options.queue)  
-        exit(1 if num_messages > options.crit_queue else 2)   
-      
-    rate = data["messages_details"]["rate"]
-      
-    last_event = data["messages_details"]["last_event"] / 1000  
-    last_event_time = time.ctime(last_event)  
-      
-    #diff = abs(last_event_time - datetime.datetime.today())  
-    #last_event_time_diff_seconds = diff.seconds + diff.days * 86400  
-    last_event_time_diff_seconds = abs(last_event - int(time.time()))  
-    if last_event_time_diff_seconds > options.crit_seconds or last_event_time_diff_seconds > options.warn_seconds:  
-        print "%s seconds since last event consumed on %s" % (last_event_time_diff_seconds, options.queue)  
-        exit(1 if last_event_time_diff_seconds > options.crit_seconds else 2)  
-      
-    print "Last event consumed: %s" % last_event_time
+    username = make_option("--username", dest="username", help="RabbitMQ API username", type="string", default="guest")
+    password = make_option("--password", dest="password", help="RabbitMQ API password", type="string", default="guest")
+    port = make_option("--port", dest="port", help="RabbitMQ API port", type="string", default="15672")
+    vhost = make_option("--vhost", dest="vhost", help="RabbitMQ vhost", type="string", default='%2F')
+    queue = make_option("--queue", dest="queue", help="Name of the queue in inspect", type="string", default="queue")
+
+
+    def doApiGet(self):
+        """
+        performs and returns content from an api get
+        """
+        password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
+        password_mgr.add_password(None, self.url, self.options.username, self.options.password)
+        handler = urllib2.HTTPBasicAuthHandler(password_mgr)
+        opener = urllib2.build_opener(handler)
+
+        response = None
+        try:
+            request = opener.open(self.url)
+            response = request.read()
+            request.close()
+        except Exception, e:
+            response = False
+            self.rabbit_error = 2
+            self.rabbit_note = "problem with api get:", e
+
+
+        return response
+
+
+    def makeQueueUrl(self):
+        """
+        forms self.url, a correct url to polling a rabbit queue
+        """
+        try:
+            self.url = "http://%s:%s/api/queues/%s/%s" % (self.options.hostname,
+                self.options.port, self.options.vhost, self.options.queue)
+            return True
+        except Exception, e:
+            self.rabbit_error = 3
+            self.rabbit_note = "problem forming api url:", e
+            return False
+
+
+    def testOptions(self):
+        """
+        returns false if necessary options aren't present
+        """
+
+        if not self.options.hostname or not self.options.port \
+            or not self.options.vhost or not self.options.queue:
+
+            return False
+
+        return True
+
+
+    def quickExit(self, v):
+        """
+        zero out the perf data and return
+        """
+
+        result = self.response_for_value(v, m)
+
+        if not self.options.queue:
+            queue = 'queue'
+        else:
+            queue = self.options.queue
+
+        result.set_perf_data(queue + "_messagess", 0)
+        result.set_perf_data(queue + "_rate", 0)
+        result.set_perf_data(queue + "_consumers", 0)
+        result.set_perf_data("rabbit_error", self.rabbit_error)
+        result.message = self.rabbit_note
+
+        return result
+
+
+    def check(self):
+        """
+        returns a response and perf data for this check
+        """
+
+
+        self.rabbit_error = 0
+        self.rabbit_note = "action performed successfully"
+
+        if not self.testOptions():
+            return self.quickExit(255)
+
+        if not self.makeQueueUrl():
+            return self.quickExit(255)
+
+        response = self.doApiGet()
+
+        if self.rabbit_error > 0:
+            return self.quickExit(255)
+
+        try:
+            data = json.loads(response)
+        except Exception, e:
+            data = None
+            self.rabbit_error = 4
+            self.rabbit_note = "problem with json parse:", e
+
+        if self.rabbit_error > 0:
+            return self.quickExit(255)
+
+        queue = self.options.queue
+
+        result = self.response_for_value(self.rabbit_error) 
+        result.set_perf_data(queue + "_messagess", data['messages'])
+        result.set_perf_data(queue + "_rate", data['messages_details']['rate'])
+        result.set_perf_data(queue + "_consumers", data['consumers'])
+        result.set_perf_data("rabbit_error", self.rabbit_error)
+        result.message = self.rabbit_note
+
+        return result
+
+
+if __name__ == "__main__":
+
+    obj = RabbitCheck()
+    obj.check().exit()
